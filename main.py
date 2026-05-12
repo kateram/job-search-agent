590d31cbcfad8fe7be4c4cbedc2f419b88af8d1b
kateram <kateram@ymail.com>
2026-05-12 13:13:30 -0400

    feature: dashborad v3 with historical view

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ async def serve_dashboard():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)t
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
